88bf8322c3227164af7054d7ac9d27917431ea99
wesleylimacosta <wesley.limacosta@hotmail.com>
2025-08-03 23:25:41 -0300

    ADD - Adicionado site estatico html,assets com imagens e javascript

---
+++ assets/js/tailwindinit.js
@@ -0,0 +1,21 @@
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: '#22c55e',
+                        dark: '#0a0a0a',
+                        light: '#e5e7eb',
+                    },
+                    fontFamily: {
+                        inter: [
+                            'Inter',
+                            '-apple-system',
+                            'BlinkMacSystemFont',
+                            'Segoe UI',
+                            'Roboto',
+                            'sans-serif',
+                        ],
+                    },
+                },
+            },
+        }
